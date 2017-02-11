a31a7ca10a2f911def3a04a50859dd024849d0c2
Shyam Kotha (shykotha) <shykotha@cisco.com>
2017-02-11 21:37:43 +0530

    Maven Test Files
    Maven Test Files

---
+++ src/main/java/com/cisco/maven/MavenApplication.java
@@ -0,0 +1,15 @@
+package com.cisco.maven;
+
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String mavenString(){
+		
+		return "Maven Application";
+	}
+
+}
---
+++ src/test/java/com/cisco/maven/MavenApplicationTest.java
@@ -0,0 +1,17 @@
+package com.cisco.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MavenApplicationTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b ==3);
+	}
+
+}
